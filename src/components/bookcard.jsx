18635e37a51075ae1d9cea711ade6877861d4042
arbin-mahato <proarbind@gmail.com>
2025-03-18 15:18:10 +0530

    Display multiple components

--- src/components/bookcard.jsx
+++ src/components/bookcard.jsx
@@ -1,1 +1,28 @@
-// write the book component code here
+// write the book component code
+import styles from "./BookCard.module.css";
+import PropTypes from "prop-types";
+
+const BookCard = ({ books }) => {
+  return (
+    <>
+      {books.map((book) => (
+        <div key={book.id} className={styles.bookCard}>
+          <img
+            src={book.image}
+            alt={book.name}
+            style={{ width: "220px", height: "300px", objectFit: "cover" }}
+          />
+          <h3>{book.name}</h3>
+          <p>Genre: {book.genre}</p>
+          <p>Author: {book.author}</p>
+        </div>
+      ))}
+    </>
+  );
+};
+
+BookCard.propTypes = {
+  books: PropTypes.array.isRequired,
+};
+
+export default BookCard;
